c1ec5b596cd772f45b753c05fdd49ff5eb9e2c7f
ronanski11 <ronanski11@outlook.com>
2024-01-23 13:16:18 +0100

    Initial commit

---
+++ src/main/java/com/roche/icta/experimental/ronanski11/dockerapi/DockerapiApplication.java
@@ -0,0 +1,13 @@
+package com.roche.icta.experimental.ronanski11.dockerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roche/icta/experimental/ronanski11/dockerapi/DockerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roche.icta.experimental.ronanski11.dockerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
